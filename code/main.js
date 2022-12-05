70d7673b4f30a2a6c7a2004448d8a21ab3b45fef
YunNamwon <qazcde87@naver.com>
2022-12-05 14:51:27 +0900

    v1

--- code/main.js
+++ code/main.js
@@ -1,7 +1,8 @@
 const API_KEY = 'y6baIZu7L2b9Bf8NsVD89DXZkSdIhXePYERESuWusnLnD7%2F7GDUy2zDqLWVvD5gCcsfl%2FzK6Qyh4IY1FkXibDA%3D%3D';
 
 async function getData(){
-    const url=`http://apis.data.go.kr/B552061/frequentzoneBicycle/getRestFrequentzoneBicycle?ServiceKey=${API_KEY}&searchYearCd=2015&siDo=11&guGun=680&type=json&numOfRows=10&pageNo=1`
+    const url=`http://apis.data.go.kr/B552061/frequentzoneBicycle/getRestFrequentzoneBicycle?ServiceKey=${API_KEY}
+    &searchYearCd=2015&siDo=11&guGun=680&type=json&numOfRows=10&pageNo=1`
     const response = await fetch(url);
     const data = await response.json();
     console.log("data", data);
